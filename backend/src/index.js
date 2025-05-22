e2384d640320bf5cc0d992c5b64f53d668b501f0
irfanhaseeb <haseebirf1@gmail.com>
2025-05-22 17:24:40 -0400

    fix: add cors package and add settings with temporary dev config

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import { config } from 'dotenv'
 
@@ -15,6 +16,12 @@ const PORT = process.env.PORT
 // Allow for json requests (post requests) in req.body
 app.use(express.json())
 app.use(cookieParser())
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }),
+)
 
 app.use('/api/auth', authRoutes)
 app.use('/api/message', messageRoutes)
