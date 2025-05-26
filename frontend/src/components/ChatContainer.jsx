9225b6297b1530a45c7513bbe2a7ca2d7e34f2e6
irfanhaseeb <haseebirf1@gmail.com>
2025-05-26 15:11:06 -0400

    feat: Add chat header container, basic message input and chat container

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,5 +1,27 @@
+import { useEffect } from 'react'
+
+import { useChatStore } from '../store/useChatStore'
+import ChatHeader from './ChatHeader'
+import MessageInput from './MessageInput'
+
 const ChatContainer = () => {
-  return <div>ChatContainer</div>
+  const { messages, getMessages, isMessagesLoading, selectedUser } = useChatStore()
+
+  useEffect(() => {
+    getMessages(selectedUser._id)
+  }, [getMessages, selectedUser._id])
+
+  if (isMessagesLoading) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+      <p>Messages...</p>
+      <MessageInput />
+    </div>
+  )
 }
 
 export default ChatContainer
