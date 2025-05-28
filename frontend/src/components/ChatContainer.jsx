74cdebe86ba13ba7a19733a2cc826ea942c956e2
irfanhaseeb <haseebirf1@gmail.com>
2025-05-27 20:03:47 -0400

    feat: add chat bubbles, functionality all working except for real-time communication

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,12 +1,16 @@
 import { useEffect } from 'react'
 
+import { useAuthStore } from '../store/useAuthStore'
 import { useChatStore } from '../store/useChatStore'
+import { formatMessageTime } from '../lib/utils'
+import avatar from '../assets/avatar.png'
 import ChatHeader from './ChatHeader'
 import MessageInput from './MessageInput'
 import MessageSkeleton from './skeletons/MessageSkeleton'
 
 const ChatContainer = () => {
   const { messages, getMessages, isMessagesLoading, selectedUser } = useChatStore()
+  const { authUser } = useAuthStore()
 
   useEffect(() => {
     getMessages(selectedUser._id)
@@ -26,7 +30,28 @@ const ChatContainer = () => {
   return (
     <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader />
-      <p>Messages...</p>
+
+      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+        {messages.map((message) => (
+          <div key={message._id} className={`chat ${message.senderId === authUser._id ? 'chat-end' : 'chat-start'}`}>
+            <div className="chat-image avatar">
+              <div className="size-10 rounded-full border">
+                <img
+                  src={message.senderId === authUser._id ? authUser.profilePic || avatar : selectedUser.profilePic || avatar}
+                  alt="Profile picture"
+                />
+              </div>
+            </div>
+            <div className="chat-header mb-1">
+              <time className="text-xs opacity-50 ml-1">{formatMessageTime(message.createdAt)}</time>
+            </div>
+            <div className="chat-bubble flex flex-col">
+              {message.image && <img src={message.image} alt="Attachment" className="sm:max-w-[200px] rounded-md mb-2" />}
+              {message.text && <p>{message.text}</p>}
+            </div>
+          </div>
+        ))}
+      </div>
       <MessageInput />
     </div>
   )
